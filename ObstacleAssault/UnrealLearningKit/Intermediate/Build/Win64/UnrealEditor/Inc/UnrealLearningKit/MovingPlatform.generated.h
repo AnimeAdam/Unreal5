b244ddf439b8e71e514709ae7c21fc01b2a8fb3a
AnimeAdam <Adam.Szlamp@gmail.com>
2022-06-29 05:19:12 +0100

    Added in new C++ Class

---
+++ ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/UnrealEditor/Inc/UnrealLearningKit/MovingPlatform.generated.h
@@ -0,0 +1,90 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNREALLEARNINGKIT_MovingPlatform_generated_h
+#error "MovingPlatform.generated.h already included, missing '#pragma once' in MovingPlatform.h"
+#endif
+#define UNREALLEARNINGKIT_MovingPlatform_generated_h
+
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_SPARSE_DATA
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_RPC_WRAPPERS
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMovingPlatform(); \
+	friend struct Z_Construct_UClass_AMovingPlatform_Statics; \
+public: \
+	DECLARE_CLASS(AMovingPlatform, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnrealLearningKit"), NO_API) \
+	DECLARE_SERIALIZER(AMovingPlatform)
+
+
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMovingPlatform(); \
+	friend struct Z_Construct_UClass_AMovingPlatform_Statics; \
+public: \
+	DECLARE_CLASS(AMovingPlatform, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/UnrealLearningKit"), NO_API) \
+	DECLARE_SERIALIZER(AMovingPlatform)
+
+
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMovingPlatform(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMovingPlatform) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMovingPlatform); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMovingPlatform); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMovingPlatform(AMovingPlatform&&); \
+	NO_API AMovingPlatform(const AMovingPlatform&); \
+public:
+
+
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMovingPlatform(AMovingPlatform&&); \
+	NO_API AMovingPlatform(const AMovingPlatform&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMovingPlatform); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMovingPlatform); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMovingPlatform)
+
+
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_9_PROLOG
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_SPARSE_DATA \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_RPC_WRAPPERS \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_INCLASS \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_SPARSE_DATA \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_INCLASS_NO_PURE_DECLS \
+	FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UNREALLEARNINGKIT_API UClass* StaticClass<class AMovingPlatform>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_UnrealLearningKit_Source_UnrealLearningKit_MovingPlatform_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/UnrealLearningKitEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30143
+#include "H:/WORK/Learning Programming-Game Engines/UnrealEngine/Unreal5/ObstacleAssault/UnrealLearningKit/Intermediate/Build/Win64/UnrealLearningKitEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
